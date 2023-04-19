0cfe6a027a5d5b5dba6ec0c1c3b97b50fbd86815
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-04-19 16:16:13 -0600

    wip

--- scripts/vm/setup_solana_dev.sh
+++ scripts/vm/setup_solana_dev.sh
@@ -7,6 +7,7 @@ cd "$HOME"
 export DEBIAN_FRONTEND=noninteractive
 
 sh -c "$(curl -sSfL https://release.solana.com/v1.15.2/install)"
+export PATH="/home/azureuser/.local/share/solana/install/active_release/bin:$PATH"
 solana --version
 
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y pkg-config build-essential libudev-dev
