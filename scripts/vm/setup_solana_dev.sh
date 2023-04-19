7bfa510759587a651e5a40a407262046167160a9
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-04-19 15:52:51 -0600

    add setup_solana_dev vm script

---
+++ scripts/vm/setup_solana_dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$HOME"
+
+export DEBIAN_FRONTEND=noninteractive
+
+sh -c "$(curl -sSfL https://release.solana.com/v1.15.2/install)"
+solana --version
+
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y pkg-config build-essential libudev-dev
+cargo install --git https://github.com/project-serum/anchor avm --locked --force
+avm install latest
+avm use latest
+anchor --version
+
+# Setup keypair
+solana-keygen new --no-bip39-passphrase -o ~/.config/solana/id.json
+ 
