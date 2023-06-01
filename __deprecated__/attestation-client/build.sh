43973e07decae26437eb0c03c0b8d2975e39a196
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-06-01 14:08:27 -0600

    cleanup

--- __deprecated__/attestation-client/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build . --tag local
-id=$(docker run -it -d --rm local bash)
-rm -rf target
-docker cp "$id":/app/target target/
--- __deprecated__/attestation-client/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod error;
-pub mod quote_verify;
-pub mod sgx;
-pub mod sgx_dcap_quoteverify_sys;
-pub mod sgx_dcap_quoteverify_rs;
-pub mod solana;
-
